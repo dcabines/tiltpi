fd471df313aed834dae7eb14110fe4ee1ccadca0
David Cabiness <dcabines@gmail.com>
2018-05-21 21:06:56 -0400

    new message

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/', (req, res) => res.send('Welcome to tilt.local'));
+app.listen(80, () => console.log('listening on port 80'));
