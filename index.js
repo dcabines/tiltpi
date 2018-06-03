3eb4ba89885c23a616f8dd06bf3aca10fbc95689
David Cabiness <dcabines@gmail.com>
2018-06-03 14:19:45 -0400

    more

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const os = require('os');
 const express = require('express');
 const app = express();
 
+const info = {
+  eol: os.EOL,
+  arch: os.arch(),
+  constants: os.constants,
+  cpus: os.cpus(),
+  freemem: os.freemem(),
+  homedir: os.homedir(),
+  hostname: os.hostname()
+};
+
 app.get('/', (req, res) => res.send('Welcome to tilt.local'));
+
+app.get('/os', (req, res) => res.send(info));
+
 app.listen(80, () => console.log('listening on port 80'));
